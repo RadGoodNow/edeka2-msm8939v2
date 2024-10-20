01148e1a15bdafa3a2dda620c1c4f5ec214dee8e
RadGoodNow <81907382+RadGoodNow@users.noreply.github.com>
2024-10-20 23:59:22 +0400

    ci-build.sh: git reset after firstrun.sh

--- ci-build.sh
+++ ci-build.sh
@@ -4,9 +4,10 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc clang
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git --recursive && cd edk2 && git reset --hard 5801910013757bd626f67ed77eea6c16a176eebf && cd ..
+git clone https://github.com/tianocore/edk2.git --recursive
 git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
 cp header.makefile ../edk2/BaseTools/Source/C/Makefiles
 ./firstrun.sh
+cd ../edk2 && git reset --hard 5801910013757bd626f67ed77eea6c16a176eebf && cd ..
 ./build_ido.sh
