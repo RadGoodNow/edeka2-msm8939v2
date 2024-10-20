bf52d71e52faca009fe160582002452eee25562a
rad <81907382+RadGoodNow@users.noreply.github.com>
2024-10-20 14:05:18 +0300

    ci-build.sh: fix git reset error

--- ci-build.sh
+++ ci-build.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git --recursive && git reset --hard 5801910
-git clone https://github.com/tianocore/edk2-platforms.git && git reset --hard d8232e8 
+git clone https://github.com/tianocore/edk2.git --recursive && cd edk2 && git reset --hard 5801910 && cd ..
+git clone https://github.com/tianocore/edk2-platforms.git && cd edk2-platforms && git reset --hard d8232e8 && cd ..
 cd "$curdir"
 ./firstrun.sh
 ./build_ido.sh
