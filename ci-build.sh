e2f9ef56a3c2dc54babfdc69e5089df5ef49c413
RadGoodNow <81907382+RadGoodNow@users.noreply.github.com>
2024-10-21 00:37:30 +0400

    TEST: ci-build.sh: don't copy header.makefile

--- ci-build.sh
+++ ci-build.sh
@@ -7,7 +7,7 @@ cd ..
 git clone https://github.com/tianocore/edk2.git --recursive
 git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
-cp header.makefile ../edk2/BaseTools/Source/C/Makefiles
+#cp header.makefile ../edk2/BaseTools/Source/C/Makefiles
 ./firstrun.sh
 cd ../edk2 && git reset --hard 5801910013757bd626f67ed77eea6c16a176eebf && cd "$curdir"
 ./build_ido.sh
