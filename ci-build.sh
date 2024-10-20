a75128af5cae440c77949f92d583d25c0fcde21c
RadGoodNow <81907382+RadGoodNow@users.noreply.github.com>
2024-10-20 23:50:51 +0400

    ci-build.sh: don't use old edk2-platforms

--- ci-build.sh
+++ ci-build.sh
@@ -5,7 +5,7 @@ sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu
 curdir="$PWD"
 cd ..
 git clone https://github.com/tianocore/edk2.git --recursive && cd edk2 && git reset --hard 5801910013757bd626f67ed77eea6c16a176eebf && cd ..
-git clone https://github.com/tianocore/edk2-platforms.git && cd edk2-platforms && git reset --hard d8232e8816fafb875a3656dda788b6681bc25149 && cd ..
+git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
 cp header.makefile ../edk2/BaseTools/Source/C/Makefiles
 ./firstrun.sh
