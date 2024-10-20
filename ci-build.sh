1ba20c086b04801dfa1490abe2f317ab45564934
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-20 12:48:03 +0200

    Update ci-build.sh

--- ci-build.sh
+++ ci-build.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd ..
-git clone -b 5801910 https://github.com/tianocore/edk2.git --recursive
-git clone -b d8232e8 https://github.com/tianocore/edk2-platforms.git 
+git clone https://github.com/tianocore/edk2.git --recursive --branch 5801910 --chosen-branch
+git clone https://github.com/tianocore/edk2-platforms.git --branch d8232e8 --chosen-branch
 cd "$curdir"
 ./firstrun.sh
 ./build_ido.sh
