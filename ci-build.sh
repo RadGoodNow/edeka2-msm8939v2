bd2dde3e2a61b3484407c27279e9795e2ed2d7b3
RadGoodNow <81907382+RadGoodNow@users.noreply.github.com>
2024-10-20 22:39:18 +0400

    ci-build.sh: use older and stable edk2

--- ci-build.sh
+++ ci-build.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git --recursive && cd edk2 && git reset --hard 5801910 && git clean -f && cd ..
-git clone https://github.com/tianocore/edk2-platforms.git && cd edk2-platforms && git reset --hard d8232e8 && git clean -f && cd ..
+git clone https://github.com/tianocore/edk2.git --recursive && cd edk2 && git reset --hard 5801910013757bd626f67ed77eea6c16a176eebf5801910013757bd626f67ed77eea6c16a176eebf && cd ..
+git clone https://github.com/tianocore/edk2-platforms.git
 cd "$curdir"
 cp header.makefile ../edk2/BaseTools/Source/C/Makefiles
 ./firstrun.sh
