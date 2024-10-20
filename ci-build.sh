71bbe41a9671e31a80bbf32243a9c4c9c974d530
rad <81907382+RadGoodNow@users.noreply.github.com>
2024-10-20 14:00:13 +0300

    ci-build.sh: attempt to clone specific commit

--- ci-build.sh
+++ ci-build.sh
@@ -4,8 +4,8 @@ sudo apt update
 sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu bc
 curdir="$PWD"
 cd ..
-git clone https://github.com/tianocore/edk2.git --recursive --branch 5801910 --chosen-branch
-git clone https://github.com/tianocore/edk2-platforms.git --branch d8232e8 --chosen-branch
+git clone https://github.com/tianocore/edk2.git --recursive && git reset --hard 5801910
+git clone https://github.com/tianocore/edk2-platforms.git && git reset --hard d8232e8 
 cd "$curdir"
 ./firstrun.sh
 ./build_ido.sh
