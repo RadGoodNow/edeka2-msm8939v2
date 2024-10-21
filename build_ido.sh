ea81d991e9ead8e151ae0259731c8605a6d22035
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-21 22:23:25 +0200

    Update build_ido.sh

--- build_ido.sh
+++ build_ido.sh
@@ -3,6 +3,10 @@
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
+cd BootShim/AARCH64 || exit 1
+make UEFI_BASE=0x94500000 UEFI_SIZE=0x00200000||_error "\nFailed to Compile BootShim!\n"
+cd ../..
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p MSM8939v2Pkg/Devices/ido.dsc
-gzip -c < workspace/Build/MSM8939v2Pkg/DEBUG_GCC5/FV/MSM8939V2PKG_UEFI.fd >uefi.img
+cat ./BootShim/AARCH64/BootShim.bin workspace/Build/MSM8939v2Pkg/DEBUG_GCC5/FV/MSM8939V2PKG_UEFI.fd > workspace/Build/MSM8939v2Pkg/DEBUG_GCC5/FV/MSM8939V2PKG_UEFI.fd-bootshim
+gzip -c < workspace/Build/MSM8939v2Pkg/DEBUG_GCC5/FV/MSM8939V2PKG_UEFI.fd-bootshim >uefi.img
 cat device_specific/ido.dtb >>uefi.img
