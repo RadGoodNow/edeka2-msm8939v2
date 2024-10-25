997effe234beb6cb1979b536cb40e50ed86ac089
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-10-25 21:44:02 +0200

    Update DeviceMemoryMap.h

--- MSM8939v2Pkg/Include/Configuration/DeviceMemoryMap.h
+++ MSM8939v2Pkg/Include/Configuration/DeviceMemoryMap.h
@@ -60,19 +60,19 @@ static ARM_MEMORY_REGION_DESCRIPTOR_EX gDeviceMemoryDescriptorEx[] = {
 //                                                    EFI_RESOURCE_ EFI_RESOURCE_ATTRIBUTE_        ARM_REGION_ATTRIBUTE_
 //MemBase,   MemSize,   MemLabel(32 Char.), BuildHob, ResourceType, ResourceAttribute, MemoryType, CacheAttributes
 
-    /* DDR Regions */
-    {"Display Reserved",  0x83000000, 0x2000000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
-    {"TZApps",            0x8c400000, 0x1900000, NoHob, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
-    {"PIL Reserved",      0x8be00000, 0x600000, AddMem, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
-    {"DXE Heap",          0x90000000, 0x04500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
-    {"UEFI FD",           0x94500000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
-    {"UEFI Stack",        0x94700000, 0x00041000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
+  // DDR Regions
+  {"Display Reserved",  0x83200000, 0x2000000,  AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
+  {"TZApps",            0x8c400000, 0x1900000,  NoHob,  SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
+  {"PIL Reserved",      0x8be00000, 0x600000,   AddMem, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
+  {"DXE Heap",          0x90000000, 0x04500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
+  {"UEFI FD",           0x94500000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
+  {"UEFI Stack",        0x94700000, 0x00041000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
 
-    /* RAM partition regions */
-    /*{"RAM Partition",     0x94741000, 0x6C400000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},*/
+  /* RAM partition regions */
+  {"RAM Partition",     0x94741000, 0x6C400000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv,   WRITE_BACK_XN},
 
-    /* Register regions */
-    {"Registers",         0x00000000, 0x80000000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
+  /* Register regions */
+  {"Registers",         0x00000000, 0x80000000, AddDev, MMAP_IO, UNCACHEABLE, MmIO,   NS_DEVICE},
 
 //------------- Terminator for MMU ----------
 {0, 0, "Terminator", 0, 0, 0, 0, 0}
